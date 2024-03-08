d95cc6d7a088fa0bc930b29137ca352496567006
mority <mority@posteo.net>
2024-03-08 15:25:29 +0100

    WIP mostly data clobbering, some routing time stats

---
+++ rourest.py
@@ -0,0 +1,143 @@
+import argparse
+import statistics as stats
+import csv
+from haversine import haversine
+
+
+def find_between(line, begin_str, end_str):
+    start = line.find(begin_str) + len(begin_str)
+    end = line.find(end_str, start)
+    return line[start:end]
+
+
+def rfind_between(line, begin_str, end_str):
+    start = line.rfind(begin_str) + len(begin_str)
+    end = line.find(end_str, start)
+    return line[start:end]
+
+
+def find_query_id(line):
+    value_str = rfind_between(line, '"id":', "}")
+    return int(value_str.strip())
+
+
+def find_routing_time(line):
+    value_str = find_between(line, '"routing_time_ms","value":', "}")
+    return int(value_str.strip())
+
+
+def find_interval_begin(line):
+    value_str = find_between(line, '"interval_begin":', ",")
+    return int(value_str.strip())
+
+
+def find_interval_end(line):
+    value_str = find_between(line, '"interval_end":', ",")
+    return int(value_str.strip())
+
+
+def find_source_id(line):
+    value_str = find_between(line, '"start": {"station": {"id": "', '"')
+    value_str = value_str.split("_")[1]
+    return value_str
+
+def find_destination_id(line):
+    value_str = find_between(line, '"destination": {"id": "', '"')
+    value_str = value_str.split("_")[1]
+    return value_str
+
+def read_stops_file(stops_file):
+    stops = {}
+    with open(stops_file, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            stops[row['stop_id']] = row
+    return stops
+
+def read_query_file(query_file):
+    query_data = {}
+    with open(query_file, "r") as file:
+        for line in file:
+            query_id = find_query_id(line)
+            source_id = find_source_id(line)
+            destination_id = find_destination_id(line)
+            query_data[query_id] = {"query_id": query_id,"source_id": source_id, "destination_id": destination_id}
+    return query_data
+
+def lookup_coordinates(query_data, stops):
+    for query in query_data:
+        query['source_lat'] = stops[query['source_id']]['stop_lat']
+        query['source_lon'] = stops[query['source_id']]['stop_lon']
+        query['destination_lat'] = stops[query['destination_id']]['stop_lat']
+        query['destination_lon'] = stops[query['destination_id']]['stop_lon']
+
+def calculate_distances(query_data):
+    for query in query_data:
+        query['distance'] = haversine((query['source_lat'],query['source_lon']),(query['destination_lat'],query['destination_lon']))
+
+
+def get_query_data(stops_file, query_file):
+    query_data = read_query_file(query_file)
+    stops = read_stops_file(stops_file)
+    lookup_coordinates(query_data,stops)
+    calculate_distances(query_data)
+    return query_data
+
+
+def get_response_data(response_file):
+    query_id_data_idx = {}  # maps query_ids to index in the data lists
+    routing_times = []  # [ms]
+    interval_sizes = []  # [s]
+    with open(response_file, "r") as file:
+        data_idx = 0
+        for line in file:
+            query_id_data_idx[find_query_id(line)] = data_idx
+            data_idx += 1
+            routing_times.append(find_routing_time(line))
+            interval_sizes.append(find_interval_end(line) - find_interval_begin(line))
+
+    return query_id_data_idx, routing_times, interval_sizes
+
+
+def get_response_stats(filename):
+    query_id_data_idx, routing_times, interval_sizes = get_response_data(filename)
+
+    # do statistics on routing times
+    results = {}
+    results["num_values"] = len(routing_times)
+    results["mean"] = stats.mean(routing_times)
+    results["min"] = min(routing_times)
+    quantiles = stats.quantiles(routing_times)
+    results["25%"] = quantiles[0]
+    results["median"] = quantiles[1]
+    results["75%"] = quantiles[2]
+    results["max"] = max(routing_times)
+
+    return results
+
+
+def print_response_stats(response_file):
+    results = get_response_stats(response_file)
+
+    # print results
+    print("--- Routing Time Statistics ---")
+    print("#values: " + str(results["num_values"]))
+    print("   mean: " + str(round(results["mean"])) + " ms")
+    print("    min: " + str(results["min"]) + " ms")
+    print("    25%: " + str(round(results["25%"])) + " ms")
+    print(" median: " + str(round(results["median"])) + " ms")
+    print("    75%: " + str(round(results["75%"])) + " ms")
+    print("    max: " + str(results["max"]) + " ms")
+    print("-------------------------------")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="ROUREST - Routing Response Statistics",
+                                     description="Statistically evaluate routing queries and the corresponding routing responses")
+    parser.add_argument("stops_file", type=str, nargs=1, help="path to the stops.txt of the GTFS timetable to lookup station coordinates")
+    parser.add_argument("query_file", type=str, nargs=1,
+                        help="path to the file containing the routing queries")
+    parser.add_argument("response_file", type=str, nargs=1,
+                        help="path to the file containing the responses to the queries")
+    args = parser.parse_args()
+    print_response_stats(args.response_file)
---
+++ test/test.py
@@ -0,0 +1,41 @@
+import unittest
+import rourest
+from statistics import mean
+from statistics import quantiles
+
+expected_query_id_data_idx = {1: 9, 2: 1, 5: 5, 6: 3, 8: 0, 12: 7, 14: 6, 15: 2, 16: 4, 20: 8}
+expected_routing_times = [360, 418, 784, 825, 889, 995, 1601, 1662, 999, 1850]
+expected_interval_sizes = [28860, 14460, 7260, 14460, 21660, 14460, 14460, 7260, 14460, 14460]
+expected_query_id_distances = {1: 298.5018236099454, 2: 214.1739625460696, 5: 244.96749861463252, 6: 53.46407769736269, 8: 181.13311046805126, 12: 14.62848431110017, 14: 209.65397955705987, 15: 398.63930912257604, 16: 156.84004368848682, 20: 450.6600236062579}
+
+class TestCase(unittest.TestCase):
+    def test_get_response_data(self):
+        query_id_data_idx, routing_times, interval_sizes = rourest.get_response_data("./responses.txt")
+        self.assertEqual(expected_query_id_data_idx, query_id_data_idx)
+        self.assertEqual(expected_routing_times, routing_times)
+        self.assertEqual(expected_interval_sizes, interval_sizes)
+
+    def test_get_stats(self):
+        results = rourest.get_response_stats("./responses.txt")
+        self.assertEqual(10, results["num_values"])
+        self.assertEqual(mean(expected_routing_times), results["mean"])
+        self.assertEqual(min(expected_routing_times), results["min"])
+        exp_quantiles = quantiles(expected_routing_times)
+        self.assertEqual(exp_quantiles[0], results["25%"])
+        self.assertEqual(exp_quantiles[1], results["median"])
+        self.assertEqual(exp_quantiles[2], results["75%"])
+        self.assertEqual(max(expected_routing_times), results["max"])
+
+    def test_print_stats(self):
+        rourest.print_response_stats("./responses.txt")
+
+    def test_read_stops_file(self):
+        rourest.read_stops_file("./stops.txt")
+
+    def test_get_query_data(self):
+        query_data = rourest.get_query_data("./stops.txt", "./queries.txt")
+        for query in query_data:
+            self.assertEqual(expected_query_id_distances[query["query_id"]], query["distance"])
+
+if __name__ == '__main__':
+    unittest.main()
